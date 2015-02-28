ab988ab055bf00c78908b37b16d78b697734dd94
sanemat <o.gata.ken@gmail.com>
2015-02-28 21:51:25 +0900

    fix(circleci): fix context

--- bin/run-rubocop.sh
+++ bin/run-rubocop.sh
@@ -3,11 +3,12 @@ set -v
 if [ "${CIRCLE_BRANCH}" != "master" ]; then
   # Circle-CI
   #
-  gem install --no-document github_status_notifier
-  github-status-notifier notify --state pending
 
   gem install --no-document rubocop-select rubocop rubocop-checkstyle_formatter \
-              checkstyle_filter-git saddler saddler-reporter-github
+              checkstyle_filter-git saddler saddler-reporter-github \
+              github_status_notifier
+
+  github-status-notifier notify --state pending --context saddler/rubocop
 
   # CircleCI stop script ;(
   # git diff -z --name-only origin/master
